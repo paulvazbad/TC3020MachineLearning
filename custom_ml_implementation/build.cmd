7799436f60250134919ffd13470096a46277c194
Paul Vazquez <pev@live.com.mx>
2021-03-20 16:14:47 -0700

    Version updated

---
+++ custom_ml_implementation/build.cmd
@@ -0,0 +1,4 @@
+cd .\custom_ml_implementation 
+.\\venv\\Scripts\\activate.bat 
+echo "Activated"
+python -m build
---
+++ custom_ml_implementation/build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cd .\\custom_ml_implementation && .\\venv\\Scripts\\activate && python -m pip build
