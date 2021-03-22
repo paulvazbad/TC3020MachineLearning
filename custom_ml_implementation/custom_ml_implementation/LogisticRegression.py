3733b29feff19a54352881c45fb36c2681367e92
Paul Vazquez <pev@live.com.mx>
2021-03-21 21:43:48 -0700

    Implemented regularization in formulas

--- custom_ml_implementation/custom_ml_implementation/LogisticRegression.py
+++ custom_ml_implementation/custom_ml_implementation/LogisticRegression.py
@@ -18,7 +18,7 @@ class LogisticRegression(LinearRegression):
     def h(self, x, theta_values):
         return(1 / (1+math.exp(- (super().h(x, theta_values)))))
 
-    def J(self, x, y, theta_values):
+    def J(self, x, y, theta_values, gamma=0):
         assert len(x) == len(y), "Length of X is not equal to length of Y"
         total_sum = 0
         m = len(x)
@@ -33,12 +33,18 @@ class LogisticRegression(LinearRegression):
             print("Theta values: " + str(theta_values))
             print("h(x) " + str(h(x[i], theta_values)))
             raise Exception("RIP")
+
+        total_regularization_sum = 0
+        if gamma:
+            for theta in theta_values:
+                total_regularization_sum+= theta*theta
+            cost +=  (gamma/(2*m)) * total_regularization_sum
+
         return cost
 
-    def J_modified(self,x,y,theta_values, feature_to_multiply):
+    def J_modified(self,x,y,theta_values, feature_to_multiply, gamma=0):
         '''
         Given a set of theta_values calculate te cost of the hypothesis h(theta) times x(i)?
-        FOR UNIVARIABLE LINEAR REGRESSION
         '''
 
         assert len(x)==len(y),"Length of X is not equal to length of Y"+str(len(x)) + " vs " +str(len(y))
@@ -46,15 +52,19 @@ class LogisticRegression(LinearRegression):
         total_sum = 0
         m = len(x)
         temp=0
+        regularization_value = 0
         for i in range(0,len(x)):
             temp=(self.h(x[i],theta_values) - y[i])
             val_to_multiply = 1
             if(feature_to_multiply > 0):
                 val_to_multiply = x[i][feature_to_multiply - 1]
-            temp = temp * val_to_multiply
+                # Add regularization if j > 0 
+                regularization_value = (gamma/m)*theta_values[feature_to_multiply]
+            temp = temp * val_to_multiply + regularization_value
             total_sum+=temp
+        total_sum = total_sum/(m)
 
-        return total_sum/(m)
+        return total_sum
 
     def predict(self,input_value,theta_values):
         return round(h(input_value, resulting_theta))
