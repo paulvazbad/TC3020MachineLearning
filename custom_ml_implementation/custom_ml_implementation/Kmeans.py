41692c15f17e8aa31f6ea5a8dcb7556ebd1a0013
Paul Vazquez <pev@live.com.mx>
2021-05-14 09:02:58 -0700

    Added function to plot clusters

--- custom_ml_implementation/custom_ml_implementation/Kmeans.py
+++ custom_ml_implementation/custom_ml_implementation/Kmeans.py
@@ -10,6 +10,8 @@ import matplotlib.pyplot as plt
 class Kmeans():
     def __init__(self):
         self.MAX_ITER = 100
+        self.K_coords = None
+        self.K = None
 
     def fit(self, K=0, examples=np.ndarray((1, 1)), max_iter=100):
         '''
@@ -31,32 +33,40 @@ class Kmeans():
         for i in range(0, self.MAX_ITER):
             print("---EPOCH: %d---" % i)
             # C
-            examples_grouped_by_cluster = []
-            
-            for i in range(0,K):
-                examples_grouped_by_cluster.append([])
+            examples_grouped_by_cluster = self.assign_cluster_to_examples(
+                K, examples, K_coords)
 
-            # Calculate to which cluster an example x(i) belongs to
-            # C[K] = [index_x1, index_x2,..., index_xn]
-            for index, example in enumerate(examples):
-                closest_cluster = self.calculate_closest_cluster(
-                    example, K_coords)
-                # Append the example in dataset of the example
-                examples_grouped_by_cluster[closest_cluster].append(index)
-            
-            cost = self.J(K_coords, examples_grouped_by_cluster,examples)    
+            cost = self.J(K_coords, examples_grouped_by_cluster, examples)
             print("Error:" + str(cost))
-            
+
             # Move the centroid of the cluster to the average of the assigned points
             for index, examples_in_cluster in enumerate(examples_grouped_by_cluster):
-                print("Examples assigned to cluster %d"%index)
-                print(examples[examples_in_cluster,:])
-                K_coords[index] = self.average_of_points(examples[examples_in_cluster,:])
+                print("Examples assigned to cluster %d" % index)
+                print(examples[examples_in_cluster, :])
+                K_coords[index] = self.average_of_points(
+                    examples[examples_in_cluster, :])
 
             print("New coords: ")
             print(K_coords)
-            
-            
+
+        self.K_coords = K_coords
+        self.K = K
+
+    def assign_cluster_to_examples(self, K, examples, K_coords):
+        examples_grouped_by_cluster = []
+
+        for i in range(0, K):
+            examples_grouped_by_cluster.append([])
+
+        # Calculate to which cluster an example x(i) belongs to
+        # C[K] = [index_x1, index_x2,..., index_xn]
+        for index, example in enumerate(examples):
+            closest_cluster = self.calculate_closest_cluster(
+                example, K_coords)
+            # Append the example in dataset of the example
+            examples_grouped_by_cluster[closest_cluster].append(index)
+
+        return examples_grouped_by_cluster
 
     def average_of_points(self, examples_grouped_by_cluster):
         '''
@@ -110,13 +120,23 @@ class Kmeans():
         '''
         return np.sum(np.square(point_a-point_b))
 
-    def predict(self):
+    def predict(self, examples=None):
         '''
         Calculates what would be the cluster to be assigned to a list of examples. 
         Receives a list of examples (with the same number of features as in thefitfunction); you can think of thislist of examples as a matrix with examples and their features.  
         This function should obtain the list of thepredicted centroids for each example (thec(i)).
         '''
         print("predict")
+        if examples is not None:
+            num_examples = examples.shape[0]
+            c = np.zeros(shape=(num_examples, 1))
+            for index, example in enumerate(examples):
+                cluster_example_belongs_to = self.calculate_closest_cluster(
+                    example, self.K_coords)
+                c[index] = cluster_example_belongs_to
+            return c
+        else:
+            print("Please pass a list of examples")
 
     def elbow(self):
         '''
@@ -132,7 +152,28 @@ class Kmeans():
         '''
         total_cost = 0
         for cluster_number, examples_in_cluster in enumerate(examples_grouped_by_cluster):
-            for example in examples[examples_in_cluster,:]:
+            for example in examples[examples_in_cluster, :]:
                 total_cost += self.distance_between_two_points(
                     K_coords[cluster_number], example)
         return total_cost
+
+    def cost(self, examples):
+        examples_grouped_by_cluster = self.assign_cluster_to_examples(
+            self.K, examples, self.K_coords)
+
+        return self.J(self.K_coords, examples_grouped_by_cluster, examples)
+
+    def plot_clusters(self, examples):
+
+        # plot the correct and incorrect
+        examples_grouped_by_cluster = self.assign_cluster_to_examples(
+                self.K, examples, self.K_coords)
+        legends = []
+        for index, cluster_examples_indexes in enumerate(examples_grouped_by_cluster):
+            examples_in_cluster_values = examples[cluster_examples_indexes, :]
+            plt.scatter(examples_in_cluster_values[:, 0], examples_in_cluster_values[:, 1])
+            legends.append(index)
+            plt.legend(legends)
+
+        plt.title("Clusters")
+        plt.show()
--- custom_ml_implementation/main.py
+++ custom_ml_implementation/main.py
@@ -1,41 +1,21 @@
 from custom_ml_implementation.LinearRegression import LinearRegression
 from custom_ml_implementation.LogisticRegression import LogisticRegression
 from custom_ml_implementation.NeuralNetwork import NeuralNetwork
+from custom_ml_implementation.Kmeans import Kmeans
 import pandas as pd
 import numpy as np
 
 train_or_load = 'load'
 
 def main():
-    # Test the XOR dataset
+    group_a = np.arange(12).reshape((6,2)) #0-11
+    group_b = np.arange(500,512).reshape((6,2)) #50-61
+    examples  = np.concatenate((group_a, group_b), axis=0)
+    examples = np.array(examples)
+    kmeans = Kmeans()
+    kmeans.fit(2,examples,10)
+    kmeans.plot_clusters(examples)
 
-    #
-
-    #[0,1]  [1]  
-    df = pd.read_csv('./custom_ml_implementation/datasets/xor.csv', sep=',')
-    features = df[['x', 'y']].values
-    labels = df['label'].values
-    
-    EPOCHS = 2500
-    LEARNING_RATE = 0.5
-    REGULARIZATION = 0.5
-    nn = NeuralNetwork(2, 1)
-    if(train_or_load=='train'):
-        nn.addHiddenLayer(4)
-        nn.train(np.array(features), np.array(labels),
-                LEARNING_RATE, REGULARIZATION, EPOCHS)
-        like_or_nah = input("Do you like the results? 1/0")
-        if(like_or_nah=='1'):
-            nn.save_model('xor_model')
-    else:
-        nn.load_model('xor_model')    
-    nn.print_weights()
-    print("Cost: ", nn.cost_function(np.array(features),np.array(labels)))
-    print(nn.predict([0.001, 0.001]), "Expected 0")
-    print(nn.predict([0.001, 1.0]), "Expected 1")
-    print(nn.predict([1.0, 0.001]), "Expected 1")
-    print(nn.predict([1.0, 1.0]), "Expected 0")
-    
 
 if __name__ == "__main__":
     main()
